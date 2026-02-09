38bf9eef6c3fa5cbf3969d3312d2e669a460bc9b
blackbird77ad <blackbird77ad@gmail.com>
2026-02-09 12:14:09 +0000

    update contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,19 @@ export default function Contact() {
     <section className="min-h-screen bg-white px-6 py-20">
       <div className="max-w-2xl mx-auto">
         <h2 className="text-4xl font-bold mb-6">Contact Us</h2>
+ 
+{/* HERO */}
+      <section className="min-h-[50vh] flex items-center bg-[#F5F5F5]">
+        <div className="max-w-5xl mx-auto px-6">
+          <h1 className="text-5xl font-semibold mb-6">
+            Get in touch
+          </h1>
+          <p className="text-gray-600 text-lg">
+            Questions, registrations, or partnerships —
+            we’d love to hear from you.
+          </p>
+        </div>
+      </section>
 
         <form
           name="contact"
